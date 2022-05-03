cf21662fb59df7ab09f4218bb0a52b24460618f6
yzanamy <harrynugroho.nugroho146@gmail.com>
2022-05-03 23:26:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/septyantoharrynugroho20190801450/MenuUtama.java
@@ -0,0 +1,14 @@
+package com.example.septyantoharrynugroho20190801450;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+    }
+}
